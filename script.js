caee2cd422b4f071bdfef06836ab9cd7f6247ef2
Abhishek9700097 <abhishek9700097@gmail.com>
2025-09-09 12:48:48 +0530

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+function about() {
+    document.querySelector('.about').scrollIntoView({ behavior: 'smooth' });
+}
+
+function skills() {
+    document.querySelector('.skills').scrollIntoView({ behavior: 'smooth' });
+}
+function contact() {
+    document.querySelector('.contact').scrollIntoView({ behavior: 'smooth' });
+}
+function certificate() {    
+    document.querySelector('.certificate').scrollIntoView({ behavior: 'smooth' });
+}
